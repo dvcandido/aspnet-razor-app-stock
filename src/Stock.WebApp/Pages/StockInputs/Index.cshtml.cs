659ed116218b5a25b5042fa24f82c80b4a45cec9
Dougals Vaz Candido <douglasvaz@hotmail.com>
2024-01-02 16:53:41 -0300

    Update date format in StockInputs/Index.cshtml

--- src/Stock.WebApp/Pages/StockInputs/Index.cshtml.cs
+++ src/Stock.WebApp/Pages/StockInputs/Index.cshtml.cs
@@ -1,26 +1,17 @@
-using System.Text;
-using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Stock.WebApp.Models;
+using Stock.WebApp.Services;
 
 namespace Stock.WebApp.Pages.StockInputs;
 
-public class IndexModel(IHttpClientFactory httpClientFactory) : PageModel
+public class IndexModel(StockInputsService service) : PageModel
 {
     [BindProperty]
     public IEnumerable<StockInput> StockInputModels { get; set; } 
 
     public async Task OnGet()
     {
-        var httpClient = httpClientFactory.CreateClient("StockApi");
-
-        using HttpResponseMessage response = await httpClient.GetAsync("stock-inputs");
-
-        if (response.IsSuccessStatusCode)
-        {
-            using var contentStream = await response.Content.ReadAsStreamAsync();
-            StockInputModels = await JsonSerializer.DeserializeAsync<IEnumerable<StockInput>>(contentStream);
-        }
+        StockInputModels = await service.GetAllAsync();
     }
 }
