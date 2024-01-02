8591a4981f2fad493780dd4a734677234288b743
Dougals Vaz Candido <douglasvaz@hotmail.com>
2024-01-02 16:54:28 -0300

    Refactor IndexModel to use ProductsService for retrieving products

--- src/Stock.WebApp/Pages/Products/Index.cshtml.cs
+++ src/Stock.WebApp/Pages/Products/Index.cshtml.cs
@@ -2,25 +2,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Stock.WebApp.Models;
+using Stock.WebApp.Services;
 
 namespace Stock.WebApp.Pages.Products;
 
-public class IndexModel(IHttpClientFactory httpClientFactory) : PageModel
+public class IndexModel(ProductsService service) : PageModel
 {
     [BindProperty]
     public IEnumerable<Product> ProductModels { get; set; }
 
     public async Task OnGet()
     {
-        var httpClient = httpClientFactory.CreateClient("StockApi");
-
-        using HttpResponseMessage response = await httpClient.GetAsync("products");
-
-        if (response.IsSuccessStatusCode)
-        {
-            using var contentStream = await response.Content.ReadAsStreamAsync();
-            ProductModels = await JsonSerializer.DeserializeAsync<IEnumerable<Product>>(contentStream);
-        }
+        ProductModels = await service.GetAllAsync();
     }
 }
 
